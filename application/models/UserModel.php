177ce6856228526aef6ecd343680f36a708fe2fb
ainadnlla <ainadrsales@gmail.com>
2018-07-15 00:15:54 +0800

    https://github.com/ainadnlla/thefourcasters

---
+++ application/models/UserModel.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class UserModel extends CI_Model{
+   
+//     public function insert($data){
+//         $this->db->insert('staff',$data);
+//     }
+//         public function view($id){
+//             $query= $this->db->get_where('staff',array('id'=>$id));
+//             return $query->row();
+//         }
+     
+       
+        
+
+//     public function insertItems($items){
+//         $this->db->insert('staff', $items);
+        
+//     }
+
+
+    public function getProd($id)
+    {
+  
+        $query = $this->db->get_where('staff', array('id' => $id));
+        return $query->row();
+  
+    }
+public function update($id,$data){
+    $this->db->where('id',$id);
+    $this->db->update('staff',$data);
+}
+
+public function delete($id,$data){
+    $this->db->where('id', $id);
+    $this->db->delete('staff');
+}
+public function getItems(){
+    $query = $this -> db->get('staff');
+    return $query->result();
+}
+
+
+public function insertUser($data){
+    $this->db->insert('staff', $data);
+}
+
+
+    
+}
+
+?>
---
+++ application/views/include/header.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+<title></title>
+
+<link href="<?=base_url()?>bs/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
