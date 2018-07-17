3aaf753523296d56bfbb4e5c5da91a567ff087e9
anewuzhere <anthonyjarlane@yahoo.com>
2018-07-17 19:37:07 +0800

    dashboard/index

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,6 +1,26 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="<?=base_url()?>bs/js/bootstrap.min.js"></script>
-    <!-- ainadnlla -->
+   <!-- <script src="<?//=base_url()?>bs/js/bootstrap.min.js"></script>-->
+    
+<script src="<?=base_url()?>dashboard/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?=base_url()?>dashboard/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="<?=base_url()?>dashboard/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=base_url()?>dashboard/dist/js/adminlte.min.js"></script>
+<!-- Sparkline -->
+<script src="<?=base_url()?>dashboard/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- jvectormap  -->
+<script src="<?=base_url()?>dashboard/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+<script src="<?=base_url()?>dashboard/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<!-- SlimScroll -->
+<script src="<?=base_url()?>dashboard/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- ChartJS -->
+<script src="<?=base_url()?>dashboard/bower_components/chart.js/Chart.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="<?=base_url()?>dashboard/dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="<?=base_url()?>dashboard/dist/js/demo.js"></script>
 </body>
 </html>
