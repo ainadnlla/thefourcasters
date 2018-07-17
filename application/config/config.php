61694335c980826e20122974d0b8a1249c68a463
anewuzhere <anthonyjarlane@yahoo.com>
2018-07-17 19:10:59 +0800

    lol

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/proto'; 
+$config['base_url'] = 'http://localhost/thefourcasters'; 
 
 /*
 |--------------------------------------------------------------------------
