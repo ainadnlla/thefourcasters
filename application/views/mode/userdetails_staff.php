888fc91edd24af9e8a2bacac6fbef86933cd56a6
anewuzhere <anthonyjarlane@yahoo.com>
2018-07-29 06:26:00 +0800

    delete modal

--- application/views/mode/userdetails_staff.php
+++ application/views/mode/userdetails_staff.php
@@ -48,10 +48,9 @@
                         <a href="<?= base_url().'user/edit/'.$emp->id?>" class="btn btn-warning" role="button">
                         <span class="glyphicon glyphicon-pencil" aria-hidden="true">
                         </span></a>
-                        <?php $onclick = array('onclick'=>"return confirm('Are you sure?')");?>
-                        <a href="<?=base_url('user/delete/'.$emp->id)?>" class="btn btn-danger" role="button">
-                        <span class="glyphicon glyphicon-trash" aria-hidden="true" name="delete" >
-                        </span></a>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-danger"><span class="glyphicon glyphicon-trash" aria-hidden="true" name="delete" >
+                        </span>
+              </button>
                         </td>
                     </tr>        
                 <?php endforeach; ?> 
@@ -68,6 +67,28 @@
         </div>        
     </section>
 </div>
+<div class="modal modal-danger fade" id="modal-danger">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title">Delete</h4>
+              </div>
+              <div class="modal-body">
+                <p>Are you sure?</p>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">No</button>
+                <a href="<?=base_url('user/del/'.$emp->id)?>"  role="button" class="btn btn-outline" >Yes</a>
+                
+              </div>
+            </div>
+            <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+        </div>
+
 
   <footer class="main-footer">
     <div class="pull-right hidden-xs">
