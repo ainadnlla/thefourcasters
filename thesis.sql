e2715d32f6b59c224d6f0b47ce288f9ea793c9a3
anewuzhere <anthonyjarlane@yahoo.com>
2018-07-28 23:53:18 +0800

    lolol

--- thesis.sql
+++ thesis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 26, 2018 at 12:40 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Generation Time: Jul 28, 2018 at 05:51 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,7 +40,37 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `name`, `username`, `password`) VALUES
-(1, 'admin', 'admin', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441');
+(1, 'admin', 'admin', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441'),
+(2, 'ane', 'ane', '87654321');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `conductor`
+--
+
+CREATE TABLE `conductor` (
+  `id` int(100) NOT NULL,
+  `image` varchar(50) NOT NULL,
+  `fname` varchar(50) NOT NULL,
+  `mname` varchar(25) DEFAULT NULL,
+  `lname` varchar(50) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `repass` varchar(10) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_type` varchar(1) NOT NULL,
+  `contact` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `conductor`
+--
+
+INSERT INTO `conductor` (`id`, `image`, `fname`, `mname`, `lname`, `email`, `password`, `repass`, `gender`, `updated`, `created`, `user_type`, `contact`) VALUES
+(2, '', 'Anthony', 'L.', 'Ane', 'blahh@yahoo.com', '12345678', '12345678', 'M', '2018-06-28 14:56:20', '2018-06-28 14:56:20', '', 2147483647);
 
 -- --------------------------------------------------------
 
@@ -50,31 +80,28 @@ INSERT INTO `admin` (`id`, `name`, `username`, `password`) VALUES
 
 CREATE TABLE `customer` (
   `id` int(100) NOT NULL,
-  `fname` varchar(50) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
   `mname` varchar(50) DEFAULT NULL,
   `lname` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `repass` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `contact` int(11) NOT NULL,
-  `gender` char(1) NOT NULL
+  `cus_type` varchar(50) NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `repass` varchar(10) NOT NULL,
+  `email` varchar(25) NOT NULL,
+  `contact` int(10) NOT NULL,
+  `gender` varchar(1) NOT NULL,
+  `status` varchar(1) NOT NULL,
+  `user_type` varchar(1) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
-INSERT INTO `customer` (`id`, `fname`, `mname`, `lname`, `password`, `repass`, `email`, `contact`, `gender`) VALUES
-(1, 'Josephina', 'Gading', 'Gongon', '123456789', '123456789', 'josephgonz@gmail.com', 917254852, 'F'),
-(2, 'Heintjie', 'V', 'Vicente', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'f7c3bc1d808e04732adf679965ccc34ca7ae3441', 'vicente@gmail.com', 917254852, 'M'),
-(3, 'Ish', 'V', 'Juanatas', '123456789', '123456789', 'irish@gmail.com', 123456789, 'F'),
-(4, 'Roman', 'De', 'Angel', '123456789', '123456789', 'romandeang@gmail.com', 123456789, 'M'),
-(5, 'Ace', 'Lag', 'Mans', '123456789', '123456789', 'acelagman@yahoo.com', 123456789, 'M'),
-(6, 'Ace', 'Lag', 'Man', '123456789', '123456789', 'acelagman@yahoo.com', 123456789, 'F'),
-(7, 'Ma', 'Vicky', 'Solomo', '123456789', '123456789', 'vicksol@yahoo.com', 231456789, 'M'),
-(8, 'Madam', 'Rosanna', 'Adao', '123456789', '123456789', 'rosadao@yahoo.com', 231456789, 'F'),
-(9, 'Maria', 'Emil', 'Jacobo', '123', '123', 'mariaemil@hotmail.com', 2147483647, 'F'),
-(10, 'Moira', 'Dela', 'Torre', '123', '123', 'moira@yahoo.com', 123456789, 'F');
+INSERT INTO `customer` (`id`, `fname`, `mname`, `lname`, `cus_type`, `password`, `repass`, `email`, `contact`, `gender`, `status`, `user_type`, `created`, `updated`) VALUES
+(2, 'Anthony', 'L.', 'Ane', '1', 'Aj_052898', 'Aj_052898', 'anewuzhere@yahoo.com', 2147483647, 'M', '', '', '2018-06-29 13:04:51', '2018-06-29 13:04:51'),
+(3, 'Rynlene', 'V', 'Ramirez', '', '89', '89', 'rrynlene@yahoo.com', 2147483647, 'F', '', '', '2018-07-27 00:37:07', '2018-07-27 00:37:07');
 
 -- --------------------------------------------------------
 
@@ -90,17 +117,9 @@ CREATE TABLE `driver` (
   `password` varchar(50) NOT NULL,
   `repass` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `contact` int(25) NOT NULL,
+  `contact` int(12) NOT NULL,
   `gender` char(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `driver`
---
-
-INSERT INTO `driver` (`id`, `fname`, `mname`, `lname`, `password`, `repass`, `email`, `contact`, `gender`) VALUES
-(1, 'Julia', 'B', 'Barretto', '123', '123', 'julbar@gmail.com', 123456789, 'F'),
-(2, 'Nadine', 'Alexis', 'Lustre', '123', '123', 'nadzlustre@gmail.com', 987654321, 'F');
 
 -- --------------------------------------------------------
 
@@ -114,35 +133,43 @@ CREATE TABLE `staff` (
   `lname` varchar(50) NOT NULL,
   `mname` varchar(50) DEFAULT NULL,
   `password` varchar(50) NOT NULL,
-  `repass` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `contact` int(25) NOT NULL,
+  `contact` int(50) NOT NULL,
   `gender` char(1) NOT NULL,
   `status` int(1) NOT NULL,
-  `usertype` int(1) NOT NULL
+  `repass` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `staff`
 --
 
-INSERT INTO `staff` (`id`, `fname`, `lname`, `mname`, `password`, `repass`, `email`, `contact`, `gender`, `status`, `usertype`) VALUES
-(2, 'Anthony Jarl', 'Ane', 'L.', 'd9b1d7db4cd6e70935368a1efb10e377', '', 'anthony_ane@icloud.com', 2147483647, 'M', 0, 0),
-(3, 'Lance', 'Manalo', 'Orquia', '123456789', '123456789', 'ljomanalo@gmail.com', 917425812, 'M', 0, 0),
-(4, 'Darleneski', 'Teves', 'Fayeski', '123456789', '123456789', 'darleneteves@gmail.com', 906258879, 'F', 0, 0),
-(6, 'Aina ', 'Sales', 'Daniella', '123456789', '123456789', 'ainadrsales@gmail.com', 917123456, 'F', 0, 0),
-(7, 'Diana', 'Yatar', 'Arianne', '123456789', '123456789', 'arianne@gmail.com', 917258478, 'F', 0, 0),
-(8, 'MK', 'Campo', 'Torre', '123456789', '123456789', 'mk@gmail.com', 925874569, 'F', 0, 0),
-(9, 'Queen', 'Pan', 'Abegail', '1234', '1234', 'abipan@gmail.com', 917123456, 'F', 0, 0);
+INSERT INTO `staff` (`id`, `fname`, `lname`, `mname`, `password`, `email`, `contact`, `gender`, `status`, `repass`) VALUES
+(3, 'Rynlene', 'Ramirez', 'V.', '12345678', 'rrynlene@yahoo.com', 45646456, 'M', 1, '12345678'),
+(4, 'Aina Daniella', 'Sales', 'R.', '123', 'ainasales@yahoo.com', 5858585, 'F', 1, '1'),
+(5, 'Anthony', 'Ane', 'L.', 'Aj_052898', 'anthonyjarlane@yahoo.com', 2147483647, 'M', 0, 'Aj_052898');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `truck`
+--
+
+CREATE TABLE `truck` (
+  `id` int(100) NOT NULL,
+  `img` int(2) NOT NULL,
+  `license_no` varchar(20) NOT NULL,
+  `insurance` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `admin`
+-- Indexes for table `conductor`
 --
-ALTER TABLE `admin`
+ALTER TABLE `conductor`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -164,32 +191,45 @@ ALTER TABLE `staff`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `truck`
+--
+ALTER TABLE `truck`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `license_no` (`license_no`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin`
+-- AUTO_INCREMENT for table `conductor`
 --
-ALTER TABLE `admin`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `conductor`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `driver`
 --
 ALTER TABLE `driver`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `staff`
 --
 ALTER TABLE `staff`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `truck`
+--
+ALTER TABLE `truck`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
