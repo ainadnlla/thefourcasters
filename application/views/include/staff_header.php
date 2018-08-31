9c88371a84ae1c59115121e79d7d29d7ef092669
anewuzhere <anthonyjarlane@yahoo.com>
2018-08-31 13:36:51 +0800

    merge

--- application/views/include/staff_header.php
+++ application/views/include/staff_header.php
@@ -56,7 +56,7 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="#" class="user-image" alt="User Image">
-              <span class="hidden-xs">STAFF NAME</span>
+              <span class="hidden-xs"><?= $this->session->name?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
