07933cd300cac69cecd18698bef1e4854c98428a
ainadnlla <ainadrsales@gmail.com>
2018-10-21 05:24:07 +0800

    chart

--- thesis.sql
+++ thesis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2018 at 03:26 PM
--- Server version: 10.1.28-MariaDB
--- PHP Version: 7.1.11
+-- Generation Time: Oct 20, 2018 at 11:23 PM
+-- Server version: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -62,10 +62,9 @@ CREATE TABLE `booking` (
   `destination` varchar(50) NOT NULL,
   `price` int(50) NOT NULL,
   `plate_no` varchar(50) NOT NULL,
-  `driver_name` varchar(50) NOT NULL,
-  `driver_lname` varchar(50) NOT NULL,
+  `drivername` varchar(50) NOT NULL,
   `driverid` int(11) NOT NULL,
-  `helper_name` varchar(50) NOT NULL,
+  `helpername` varchar(50) NOT NULL,
   `helperid` int(100) NOT NULL,
   `action` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -74,18 +73,18 @@ CREATE TABLE `booking` (
 -- Dumping data for table `booking`
 --
 
-INSERT INTO `booking` (`id`, `waybill`, `date`, `cust_type`, `custname`, `cargo`, `product`, `description`, `pieces`, `destinationid`, `destination`, `price`, `plate_no`, `driver_name`, `driver_lname`, `driverid`, `helper_name`, `helperid`, `action`) VALUES
-(1, 1664, '2018-10-03', 'Broker/Agent', 'Wuerth Phils. Inc', 'FCL 1x40', 'Paper Products', 'Printing paper, paper labels, cardboard boxes and wood pellets', 100, 1, 'MIP - Laguna, Binan (Wuerth Phils. Inc. Warehouse)', 18000, 'ABC 8771', 'Remante', '', 14, 'Virgilio Jaballa', 12, 1),
-(2, 9673, '2018-09-29', '', 'Breakthru Cleaning Agent', 'LCL (ELF)', 'Clothing', 'Not fragile', 150, 9, 'MIP - Manila-North, Valenzuela, Ugong', 12000, 'RLP 377', 'Jeremy Lansang', '', 8, 'Nino Altamia', 21, 1),
-(3, 1643, '2018-10-04', '', 'Alysons Chemical Enterprises Inc.', 'FCL 1x45', 'Frozen Goods', 'San Miguel Tender Juicy Hotdogs', 500, 38, 'MIP - Manila-South, Taguig, Bicutan', 9000, 'RLP 377', 'Joshua Patino', '', 4, 'Elembert Rodriguez', 5, 0),
-(4, 8432, '2018-09-29', '', 'MyWine - the best italian wine', 'FCL 1x20', 'Liquior and Beverages', 'Wine from Italy', 50, 19, 'MIP - Bulacan, Guiguinto', 13500, '130104', 'Jeremy Lansang', '', 8, 'Nino Altamia', 21, 2),
-(5, 2570, '2018-11-01', '', 'Continental Cargo Carriers Inc. ', 'LCL 1x40', 'Hazardous substances', 'Hazardous substances, such as explosives or chemicals. Handle with care.', 99, 9, 'MIP - Manila-North, Valenzuela, Ugong', 13500, 'ABA 9575', 'Joshua', '', 4, 'Jason Ablazo', 5, 0),
-(6, 4962, '2018-10-05', '', 'Angelogistics Co. Ltd - Pier Dispatch ', 'LCL 1x40', 'Furnitures', 'Sofa Set ', 3, 38, 'MIP - Manila-South, Taguig, Bicutan', 10000, 'RNK 277', 'Ernesto Baquiran', '', 15, 'Lito Catalan Jr.', 15, 1),
-(7, 1619, '2018-10-09', '', 'Marilou R. Arevalo (Bong)', 'FCL 1x20', 'Insulation Foam and Roof', '2x2mm Roof and Foam 19x67', 1, 16, 'MIP - Bulacan, Sta. Maria (Coastline Warehouse)', 5000, 'RLP 377', 'Jing Baluran', '', 16, 'Dario Traquina', 2, 0),
-(8, 2623, '2018-10-06', '', 'Marilou R. Arevalo (Bong)', 'FCL 1x20', 'Insulation Foam and Roof', '2x2mm Roof and Foam 19x67', 3, 34, 'MIP - Caloocan, Baesa', 19590, '130104', 'Jeremy Lansang', '', 8, 'Nino Altamia', 21, 1),
-(9, 5190, '2018-10-17', '', 'Wacker Machines Supplies Corp.', 'FCL 2x20', 'Machinery Supplies', 'Machiner Supplies by NBS14', 50, 37, 'MIP - Manila , Mandaluyong', 25000, 'UIM 308', 'Remante Ibanez', '', 14, 'Virgilio Jaballa', 12, 1),
-(10, 1809, '2018-10-05', '', 'Coastline Truck Center', 'LCL 1x40', 'Dispenser Material', 'H2O Water Dispenser Aquabest', 100, 0, 'MIP - Bulacan, Sta. Maria (Coastline Warehouse)', 20000, 'ABF 3040', 'Ernesto Baquiran', '', 15, 'Berto Lozada', 7, 1),
-(11, 15124, '2018-10-20', 'Local/Sub-contractor', 'Altron Logistics Inc.', 'LCL (ELF)', 'Frozen Goods', 'Chilled Goods', 10, 11, 'MIP - Pampanga, San Fernando', 1000, 'ABA 9575', 'Jeremy Lansang', '', 8, 'Nino Altamia', 21, 0);
+INSERT INTO `booking` (`id`, `waybill`, `date`, `cust_type`, `custname`, `cargo`, `product`, `description`, `pieces`, `destinationid`, `destination`, `price`, `plate_no`, `drivername`, `driverid`, `helpername`, `helperid`, `action`) VALUES
+(1, 1664, '2018-10-03', 'Broker/Agent', 'Wuerth Phils. Inc', 'FCL 1x40', 'Paper Products', 'Printing paper, paper labels, cardboard boxes and wood pellets', 100, 1, 'MIP - Laguna, Binan (Wuerth Phils. Inc. Warehouse)', 18000, 'ABC 8771', 'Remante', 14, 'Virgilio Jaballa', 12, 1),
+(2, 9673, '2018-09-29', '', 'Breakthru Cleaning Agent', 'LCL (ELF)', 'Clothing', 'Not fragile', 150, 9, 'MIP - Manila-North, Valenzuela, Ugong', 12000, 'RLP 377', 'Jeremy Lansang', 8, 'Nino Altamia', 21, 1),
+(3, 1643, '2018-10-04', '', 'Alysons Chemical Enterprises Inc.', 'FCL 1x45', 'Frozen Goods', 'San Miguel Tender Juicy Hotdogs', 500, 38, 'MIP - Manila-South, Taguig, Bicutan', 9000, 'RLP 377', 'Joshua Patino', 4, 'Elembert Rodriguez', 5, 0),
+(4, 8432, '2018-09-29', '', 'MyWine - the best italian wine', 'FCL 1x20', 'Liquior and Beverages', 'Wine from Italy', 50, 19, 'MIP - Bulacan, Guiguinto', 13500, '130104', 'Jeremy Lansang', 8, 'Nino Altamia', 21, 2),
+(5, 2570, '2018-11-01', '', 'Continental Cargo Carriers Inc. ', 'LCL 1x40', 'Hazardous substances', 'Hazardous substances, such as explosives or chemicals. Handle with care.', 99, 9, 'MIP - Manila-North, Valenzuela, Ugong', 13500, 'ABA 9575', 'Joshua', 4, 'Jason Ablazo', 5, 0),
+(6, 4962, '2018-10-05', '', 'Angelogistics Co. Ltd - Pier Dispatch ', 'LCL 1x40', 'Furnitures', 'Sofa Set ', 3, 38, 'MIP - Manila-South, Taguig, Bicutan', 10000, 'RNK 277', 'Ernesto Baquiran', 15, 'Lito Catalan Jr.', 15, 1),
+(7, 1619, '2018-10-09', '', 'Marilou R. Arevalo (Bong)', 'FCL 1x20', 'Insulation Foam and Roof', '2x2mm Roof and Foam 19x67', 1, 16, 'MIP - Bulacan, Sta. Maria (Coastline Warehouse)', 5000, 'RLP 377', 'Jing Baluran', 16, 'Dario Traquina', 2, 0),
+(8, 2623, '2018-10-06', '', 'Marilou R. Arevalo (Bong)', 'FCL 1x20', 'Insulation Foam and Roof', '2x2mm Roof and Foam 19x67', 3, 34, 'MIP - Caloocan, Baesa', 19590, '130104', 'Jeremy Lansang', 8, 'Nino Altamia', 21, 1),
+(9, 5190, '2018-10-17', '', 'Wacker Machines Supplies Corp.', 'FCL 2x20', 'Machinery Supplies', 'Machiner Supplies by NBS14', 50, 37, 'MIP - Manila , Mandaluyong', 25000, 'UIM 308', 'Remante Ibanez', 14, 'Virgilio Jaballa', 12, 1),
+(10, 1809, '2018-10-05', '', 'Coastline Truck Center', 'LCL 1x40', 'Dispenser Material', 'H2O Water Dispenser Aquabest', 100, 0, 'MIP - Bulacan, Sta. Maria (Coastline Warehouse)', 20000, 'ABF 3040', 'Ernesto Baquiran', 15, 'Berto Lozada', 7, 1),
+(11, 15124, '2018-10-20', 'Local/Sub-contractor', 'Altron Logistics Inc.', 'LCL (ELF)', 'Frozen Goods', 'Chilled Goods', 10, 11, 'MIP - Pampanga, San Fernando', 1000, 'ABA 9575', 'Jeremy Lansang', 8, 'Nino Altamia', 21, 0);
 
 -- --------------------------------------------------------
 
@@ -347,31 +346,31 @@ CREATE TABLE `maintenance` (
 --
 
 INSERT INTO `maintenance` (`id`, `date`, `plate_no`, `supplier`, `description`, `purchased`, `price`, `unit`, `quantity`, `amount`, `warning`) VALUES
-(1, '2018-10-20 12:32:40', 'RLP 377', 'T.Y ENTERPRISES', 'Plasmaglow Replacement Xenon Headlight Bulbs24V', '2018-10-27', 10000, 'PCS', 2, NULL, 'HALOGEN BULB H4 24V NARVA \r\n'),
-(2, '2018-10-20 12:42:32', 'RLP 377', 'T.Y ENTERPRISES', 'MITSUBISHI FUSO OEM TRUCK (L/H) COMBINATION MIRROR', '2018-10-01', 2000, 'PCS', 10, NULL, 'GUTTEN MIRROR FUSO\r\n'),
-(3, '2018-10-20 12:45:04', 'RLP 377', 'T.Y ENTERPRISES', 'A rear drum brake on a Kawasaki W800 Truck', '2018-10-04', 5500, 'PCS', 4, NULL, 'BRAKE DRUM\r\n'),
-(4, '2018-10-20 12:49:30', 'RLP 377', 'BOLTS & NUTS HARDWARE CORP', 'CS 8X40 WITH NUT  LW FW ', '2018-10-08', 5, 'PCS', 8, NULL, 'Fully Threaded Studs - Rods and Studs'),
-(5, '2018-10-20 12:51:26', 'RLP 377', 'T.Y ENTERPRISES', 'Light Side Marker Lights', '2018-10-10', 250, 'PCS', 1, NULL, 'SIGNAL LIGHT LENS \r\n'),
-(6, '2018-10-20 12:52:55', 'ABC 8771', 'T.Y ENTERPRISES', 'TAIL LIGHT LH&RH ', '2018-09-01', 1200, 'PCS', 1, NULL, 'Brake Light Tailight Tail Lamp LH & RH'),
-(7, '2018-10-20 12:54:50', 'ABC 8771', 'T.Y ENTERPRISES', 'National brake fluid, gear oils, engine oils, brak', '2018-09-05', 185, 'PCS', 1, NULL, 'BRAKE FLUID NATIONAL\r\n'),
-(8, '2018-10-20 12:56:16', 'ABC 8771', 'T.Y ENTERPRISES', 'Multifunctional side mirror with TS16949 Model NO', '2018-08-04', 350, 'PCS', 2, NULL, 'SIDE MIRROR\r\n'),
-(9, '2018-10-20 12:57:46', 'ABC 8771', 'AVISLEY HARDWARE CORP', ' Air Cuff Lock covers and locks the truck', '2018-08-21', 75, 'PCS', 1, NULL, 'PADLOCK\r\n'),
-(10, '2018-10-20 12:59:44', 'ABF 3040', 'AVISLEY HARDWARE CORP', 'Dodge Ram Truck Remote Key Fobik w/ Engine Start.', '2018-07-02', 100, 'PCS', 1, NULL, 'KEY\r\n'),
-(11, '2018-10-20 13:01:19', 'ABF 3040', 'T.Y ENTERPRISES', 'Alagoo 12v 6\'\'car Cooling Fan Automobile Vehicle C', '2018-07-10', 350, 'PCS', 1, NULL, 'CAR -FAN 24L 6\r\n'),
-(12, '2018-10-20 13:02:22', 'ABF 3040', 'T.Y ENTERPRISES', '501 Twenty20 Cree LED 12V W5W Canbus Wedge Bulb.', '2018-07-31', 80, 'PCS', 6, NULL, 'SIDE LIGHT LED \r\n'),
-(13, '2018-10-20 13:03:58', '130104', 'BOLTS & NUTS HARDWARE CORP', 'LED Bulb 102V', '2018-06-22', 250, 'PCS', 2, NULL, 'HANGEN BULB H-4 \r\n'),
-(14, '2018-10-20 13:06:39', '130104', 'PETRUBI MARKETING CORP', 'Mark and drill four 0.50-in. diameter holes in tru', '2018-05-04', 780, 'PCS', 1, NULL, 'ANGLE 1/4 X 2 \r\n'),
-(15, '2018-10-20 13:09:06', '130104', 'GOLDIBUE MARKETING CORP', 'GUL 2x2mm 19x67', '2018-10-11', 1400, 'PCS', 1, NULL, 'GUL PLUS 16X4X8 \r\n'),
-(16, '2018-10-20 13:11:02', '130205', 'GOLD - EVER MERCHANDISING CORP', 'Hot Sale 32ml glass measuring cylinder for diesel ', '2018-09-17', 1, 'PCS', 100, NULL, 'PSBWCH \r\n'),
-(17, '2018-10-20 13:13:58', '130205', 'T.Y ENTERPRISES', 'GE 53 2watt, 14.4volts, G3.5 (G3 1/2) Bulb Type, M', '2018-07-29', 25, 'PCS', 10, NULL, 'AUTO BULB 53\r\n'),
-(18, '2018-10-20 13:15:20', 'RCE 120', 'AVISLEY HARDWARE CORP', 'POWER/HDD LED Connector Cables 18.5\"', '2018-10-14', 650, 'PCS', 1, NULL, 'FRONT PANEL CABLE \r\n'),
-(19, '2018-10-20 13:16:29', 'RCE 120', 'AVISLEY HARDWARE CORP', 'Flange Bolt 6x25 Honda OEM 95701-0602508', '2018-09-26', 6, 'PCS', 20, NULL, 'BOLT 6X25 COMPLETE \r\n'),
-(20, '2018-10-20 13:17:56', 'RCE 120', 'T.Y ENTERPRISES', 'DC12V Door Drop Bolt Lock 1 x DC 12V Electric Drop', '2018-09-11', 45, 'PCS', 1, NULL, 'DROP BOLT \r\n'),
-(21, '2018-10-20 13:18:46', 'RCE 120', 'PETRUBI MARKETING CORP', 'A1121218 1/2 X 1/2 X 1/8', '2018-10-02', 600, 'PCS', 1, NULL, 'ANGLE 3/4 X 2\r\n'),
-(22, '2018-10-20 13:19:52', 'RCE 120', 'T.Y ENTERPRISES', 'Led Kit, Bulbs Rxv Ezgo Light Complete', '2018-07-27', 20, 'PCS', 1, NULL, 'BULB 17063\r\n'),
-(23, '2018-10-20 13:21:16', 'RCE 120', 'BOLTS&NUTS HARDWARE CORP.', ' Flanged Lock Screw (Hexagon) ', '2018-07-17', 10, 'PCS', 1, NULL, '3/X / 1/2 CS WITH NUT FW\r\n'),
-(24, '2018-10-20 13:22:34', 'RCE 120', 'T.Y ENTERPRISES', '3600A Drum, Trailer Brake Drums', '2018-09-22', 7200, 'PCS', 2, NULL, 'BRAKE DRUM'),
-(25, '2018-10-20 13:24:01', 'ABF 3040', 'T.Y ENTERPRISES', 'MITSUBISHI FUSO OEM TRUCK (L/H) COMBINATION MIRROR', '2018-06-17', 350, 'PCS', 1, NULL, 'GUTTEN MIRROR FUSO\r\n');
+(1, '2018-10-20 04:32:40', 'RLP 377', 'T.Y ENTERPRISES', 'Plasmaglow Replacement Xenon Headlight Bulbs24V', '2018-10-27', 10000, 'PCS', 2, NULL, 'HALOGEN BULB H4 24V NARVA \r\n'),
+(2, '2018-10-20 04:42:32', 'RLP 377', 'T.Y ENTERPRISES', 'MITSUBISHI FUSO OEM TRUCK (L/H) COMBINATION MIRROR', '2018-10-01', 2000, 'PCS', 10, NULL, 'GUTTEN MIRROR FUSO\r\n'),
+(3, '2018-10-20 04:45:04', 'RLP 377', 'T.Y ENTERPRISES', 'A rear drum brake on a Kawasaki W800 Truck', '2018-10-04', 5500, 'PCS', 4, NULL, 'BRAKE DRUM\r\n'),
+(4, '2018-10-20 04:49:30', 'RLP 377', 'BOLTS & NUTS HARDWARE CORP', 'CS 8X40 WITH NUT  LW FW ', '2018-10-08', 5, 'PCS', 8, NULL, 'Fully Threaded Studs - Rods and Studs'),
+(5, '2018-10-20 04:51:26', 'RLP 377', 'T.Y ENTERPRISES', 'Light Side Marker Lights', '2018-10-10', 250, 'PCS', 1, NULL, 'SIGNAL LIGHT LENS \r\n'),
+(6, '2018-10-20 04:52:55', 'ABC 8771', 'T.Y ENTERPRISES', 'TAIL LIGHT LH&RH ', '2018-09-01', 1200, 'PCS', 1, NULL, 'Brake Light Tailight Tail Lamp LH & RH'),
+(7, '2018-10-20 04:54:50', 'ABC 8771', 'T.Y ENTERPRISES', 'National brake fluid, gear oils, engine oils, brak', '2018-09-05', 185, 'PCS', 1, NULL, 'BRAKE FLUID NATIONAL\r\n'),
+(8, '2018-10-20 04:56:16', 'ABC 8771', 'T.Y ENTERPRISES', 'Multifunctional side mirror with TS16949 Model NO', '2018-08-04', 350, 'PCS', 2, NULL, 'SIDE MIRROR\r\n'),
+(9, '2018-10-20 04:57:46', 'ABC 8771', 'AVISLEY HARDWARE CORP', ' Air Cuff Lock covers and locks the truck', '2018-08-21', 75, 'PCS', 1, NULL, 'PADLOCK\r\n'),
+(10, '2018-10-20 04:59:44', 'ABF 3040', 'AVISLEY HARDWARE CORP', 'Dodge Ram Truck Remote Key Fobik w/ Engine Start.', '2018-07-02', 100, 'PCS', 1, NULL, 'KEY\r\n'),
+(11, '2018-10-20 05:01:19', 'ABF 3040', 'T.Y ENTERPRISES', 'Alagoo 12v 6\'\'car Cooling Fan Automobile Vehicle C', '2018-07-10', 350, 'PCS', 1, NULL, 'CAR -FAN 24L 6\r\n'),
+(12, '2018-10-20 05:02:22', 'ABF 3040', 'T.Y ENTERPRISES', '501 Twenty20 Cree LED 12V W5W Canbus Wedge Bulb.', '2018-07-31', 80, 'PCS', 6, NULL, 'SIDE LIGHT LED \r\n'),
+(13, '2018-10-20 05:03:58', '130104', 'BOLTS & NUTS HARDWARE CORP', 'LED Bulb 102V', '2018-06-22', 250, 'PCS', 2, NULL, 'HANGEN BULB H-4 \r\n'),
+(14, '2018-10-20 05:06:39', '130104', 'PETRUBI MARKETING CORP', 'Mark and drill four 0.50-in. diameter holes in tru', '2018-05-04', 780, 'PCS', 1, NULL, 'ANGLE 1/4 X 2 \r\n'),
+(15, '2018-10-20 05:09:06', '130104', 'GOLDIBUE MARKETING CORP', 'GUL 2x2mm 19x67', '2018-10-11', 1400, 'PCS', 1, NULL, 'GUL PLUS 16X4X8 \r\n'),
+(16, '2018-10-20 05:11:02', '130205', 'GOLD - EVER MERCHANDISING CORP', 'Hot Sale 32ml glass measuring cylinder for diesel ', '2018-09-17', 1, 'PCS', 100, NULL, 'PSBWCH \r\n'),
+(17, '2018-10-20 05:13:58', '130205', 'T.Y ENTERPRISES', 'GE 53 2watt, 14.4volts, G3.5 (G3 1/2) Bulb Type, M', '2018-07-29', 25, 'PCS', 10, NULL, 'AUTO BULB 53\r\n'),
+(18, '2018-10-20 05:15:20', 'RCE 120', 'AVISLEY HARDWARE CORP', 'POWER/HDD LED Connector Cables 18.5\"', '2018-10-14', 650, 'PCS', 1, NULL, 'FRONT PANEL CABLE \r\n'),
+(19, '2018-10-20 05:16:29', 'RCE 120', 'AVISLEY HARDWARE CORP', 'Flange Bolt 6x25 Honda OEM 95701-0602508', '2018-09-26', 6, 'PCS', 20, NULL, 'BOLT 6X25 COMPLETE \r\n'),
+(20, '2018-10-20 05:17:56', 'RCE 120', 'T.Y ENTERPRISES', 'DC12V Door Drop Bolt Lock 1 x DC 12V Electric Drop', '2018-09-11', 45, 'PCS', 1, NULL, 'DROP BOLT \r\n'),
+(21, '2018-10-20 05:18:46', 'RCE 120', 'PETRUBI MARKETING CORP', 'A1121218 1/2 X 1/2 X 1/8', '2018-10-02', 600, 'PCS', 1, NULL, 'ANGLE 3/4 X 2\r\n'),
+(22, '2018-10-20 05:19:52', 'RCE 120', 'T.Y ENTERPRISES', 'Led Kit, Bulbs Rxv Ezgo Light Complete', '2018-07-27', 20, 'PCS', 1, NULL, 'BULB 17063\r\n'),
+(23, '2018-10-20 05:21:16', 'RCE 120', 'BOLTS&NUTS HARDWARE CORP.', ' Flanged Lock Screw (Hexagon) ', '2018-07-17', 10, 'PCS', 1, NULL, '3/X / 1/2 CS WITH NUT FW\r\n'),
+(24, '2018-10-20 05:22:34', 'RCE 120', 'T.Y ENTERPRISES', '3600A Drum, Trailer Brake Drums', '2018-09-22', 7200, 'PCS', 2, NULL, 'BRAKE DRUM'),
+(25, '2018-10-20 05:24:01', 'ABF 3040', 'T.Y ENTERPRISES', 'MITSUBISHI FUSO OEM TRUCK (L/H) COMBINATION MIRROR', '2018-06-17', 350, 'PCS', 1, NULL, 'GUTTEN MIRROR FUSO\r\n');
 
 -- --------------------------------------------------------
 
